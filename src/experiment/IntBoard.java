97ae7faa732861a1518da0cce2ddb025312fe5c6
TylerBuildTools <tzunconfigured@null.spigotmc.org>
2019-02-28 15:22:54 -0700

    Removed some errors still working on the last errors for C12A2

--- src/experiment/IntBoard.java
+++ src/experiment/IntBoard.java
@@ -7,24 +7,31 @@ public class IntBoard {
 	private Set<BoardCell> visited;
 	private Set<BoardCell> targets;
 	private BoardCell[][] grid;
+	final int NUM_ROWS = 4;
+	final int NUM_COLS = 4;
 	public IntBoard() {
+		adjMtx= new HashMap<BoardCell, Set<BoardCell>>();
+		grid = new BoardCell[NUM_ROWS][NUM_COLS];
+		visited = new HashSet<BoardCell>();
+		targets = new HashSet<BoardCell>();
 		calcAdjacencies();
 	}
 	private void calcAdjacencies() {
-		for (int i = 0; i < 23; i++) {
-			for (int j = 0; j < 22; j++) {
+		for (int i = 0; i < NUM_ROWS; i++) {
+			for (int j = 0; j < NUM_COLS; j++) {
+				grid[i][j] = new BoardCell(i,j);
 				Set<BoardCell> temp = new HashSet<BoardCell>();
 				
 				if((i-1)>-1) {
 					temp.add(new BoardCell(i-1,j));
 				}
-				if((i+1)<24) {
+				if((i+1)<NUM_ROWS+1) {
 					temp.add(new BoardCell(i+1,j));
 				}
 				if((j-1)>-1) {
 					temp.add(new BoardCell(i,j-1));
 				}
-				if((j+1)<23) {
+				if((j+1)<NUM_COLS+1) {
 					temp.add(new BoardCell(i,j+1));
 				}
 				adjMtx.put(new BoardCell(i,j), temp);
@@ -32,14 +39,30 @@ public class IntBoard {
 		}
 		
 	}
-	private Set<BoardCell> getAdj(BoardCell input){
+	public Set<BoardCell> getAdj(BoardCell input){
 		return adjMtx.get(input);
 	}
 	private void calcTarget(BoardCell startCell, int pathLength) {
+		visited.clear();
+		visited.add(startCell);
 		
+		
+	}
+	private void findAllTargets(BoardCell startCell,int pathLength) {
+		for(BoardCell adjCell: adjMtx.get(startCell)) {
+			if(!visited.contains(adjCell)) {
+				visited.add(adjCell);
+				if(pathLength==1) targets.add(adjCell);
+				else findAllTargets(adjCell,pathLength-1);
+				visited.remove(adjCell);
+			}
+		}
 	}
 	private Set<BoardCell> getTargets(){
 		return targets;
+	}
+	public BoardCell getCell(int i, int j) {
+		return grid[i][j];
 	}
 	
 	
--- src/tests/IntBoardTests.java
+++ src/tests/IntBoardTests.java
@@ -1,30 +1,28 @@
 package tests;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.Assert.*;
 import java.util.Set;
 
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import experiment.BoardCell;
 import experiment.IntBoard;
 
-class IntBoardTests {
+public class IntBoardTests {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	IntBoard board;  // constructor should call calcAdjacencies() so you can test them
+	
 	@Before
     public void beforeAll() {
-       IntBoard board = new IntBoard();  // constructor should call calcAdjacencies() so you can test them
+       board = new IntBoard();
     }
+	
 	@Test
 	public void testAdjacency0()
 	{
 		BoardCell cell = board.getCell(0,0);
-		Set<BoardCell> testList = board.getAdjList(cell);
+		Set<BoardCell> testList = board.getAdj(cell);
 		assertTrue(testList.contains(board.getCell(1, 0)));
 		assertTrue(testList.contains(board.getCell(0, 1)));
 		assertEquals(2, testList.size());
