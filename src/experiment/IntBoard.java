e365ce375615516b4701f623edaf268f246b96cb
amacdougall2 <38050064+amacdougall2@users.noreply.github.com>
2019-02-28 14:35:08 -0700

    Wrote getadjs and calcadjs

--- src/experiment/IntBoard.java
+++ src/experiment/IntBoard.java
@@ -11,6 +11,40 @@ public class IntBoard {
 		calcAdjacencies();
 	}
 	private void calcAdjacencies() {
+		for (int i = 0; i < 23; i++) {
+			for (int j = 0; j < 22; j++) {
+				Set<BoardCell> temp = new HashSet<BoardCell>();
+				
+				if((i-1)>-1) {
+					temp.add(new BoardCell(i-1,j));
+				}
+				if((i+1)<24) {
+					temp.add(new BoardCell(i+1,j));
+				}
+				if((j-1)>-1) {
+					temp.add(new BoardCell(i,j-1));
+				}
+				if((j+1)<23) {
+					temp.add(new BoardCell(i,j+1));
+				}
+				adjMtx.put(new BoardCell(i,j), temp);
+			}
+		}
+		
+	}
+	private Set<BoardCell> getAdj(BoardCell input){
+		return adjMtx.get(input);
+	}
+	private void calcTarget(BoardCell startCell, int pathLength) {
+		
+	}
+	private Set<BoardCell> getTargets(){
+		return targets;
+	}
+	
+	
+	/* Failed Tests Below 
+	private void calcAdjacencies() {
 		// TODO Auto-generated method stub
 		
 	}
@@ -21,7 +55,8 @@ public class IntBoard {
 		
 	}
 	private Set<BoardCell> getTargets(){
-		return targets;
+		return null;
 	}
+	 */
 	
 }
