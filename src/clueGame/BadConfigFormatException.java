7faf90ff8a0f679364b64480fbff1dd2aad12ae7
amacdougall2 <38050064+amacdougall2@users.noreply.github.com>
2019-03-05 14:36:16 -0700

    Added comments and a BadConfigFormatException

---
+++ src/clueGame/BadConfigFormatException.java
@@ -0,0 +1,15 @@
+package clueGame;
+
+public class BadConfigFormatException extends Exception {
+
+	public BadConfigFormatException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public BadConfigFormatException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
